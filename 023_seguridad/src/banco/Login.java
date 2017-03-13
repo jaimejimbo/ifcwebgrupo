594bb222c214b6d1cbedac37924d2b9285ef1f7a
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2017-03-13 22:28:04 +0100

    Pongo al dia la rama (#15)
    * aa
    
    * Servlets.
    
    * Clase Password para encriptar las contraseñas.
    
    * Añadida libreria externa para encriptar las contraseñas con bcrypt.
    
    * - Modificada la base de datos para poder guardar los Salt.
    - Creo un procedimiento para obtener el salt.
    
    * Añadido procedimiento para comprobar login. Devuelve un entero.

---
+++ 023_seguridad/src/banco/Login.java
@@ -0,0 +1,52 @@
+package banco;
+
+import java.io.IOException;
+import java.sql.Connection;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.PreparedStatement;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet({ "/Login", "/login.php", "/login.html" })
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection con;
+	private PreparedStatement pstmt;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email");
+		String salt = BCrypt.gensalt(16);
+		String pwd = BCrypt.hashpw(request.getParameter("pwd"), salt);
+		System.out.println(salt);
+		System.out.println(pwd);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ 023_seguridad/src/banco/Registrar.java
@@ -0,0 +1,49 @@
+package banco;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * Servlet implementation class Registrar
+ */
+@WebServlet({ "/Registrar", "/registrar.php", "/registrar.html" })
+public class Registrar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Registrar() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email");
+		String salt = BCrypt.gensalt(16);
+		String pwd = BCrypt.hashpw(request.getParameter("pwd"), salt);
+		HttpSession sesion = request.getSession();
+		sesion.setAttribute("email", email);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
